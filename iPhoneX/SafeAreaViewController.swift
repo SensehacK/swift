82d6d3b4b74c2fff76894a173c4bd8b44519359c
Kautilya <kautilya.save@pwc.com>
2018-10-25 12:14:46 +0530

    Safe Area setting Buttons Programmatically.

--- iPhoneX/SafeAreaViewController.swift
+++ iPhoneX/SafeAreaViewController.swift
@@ -13,7 +13,9 @@ class SafeAreaViewController : UIViewController {
     
     @IBOutlet weak var yoLabel: UILabel!
     @IBOutlet weak var eodLabel: UILabel!
+    @IBOutlet weak var helloButton: UIButton!
     
+    var i = 0
     
     let prevButton : UIButton = {
         let button = UIButton(type: .system)
@@ -28,6 +30,7 @@ class SafeAreaViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello Safe Area")
+        print("Before Explicit setting Axis", helloButton.frame.origin.y)
         setupView()
     }
     
@@ -35,7 +38,7 @@ class SafeAreaViewController : UIViewController {
     func setupView () {
         
         setupBottomView()
-        
+        print("Hell 2 ", helloButton.frame.origin.y)
     }
     
     
@@ -47,6 +50,40 @@ class SafeAreaViewController : UIViewController {
         
         view.addSubview(bottomButton)
         bottomButton.backgroundColor = UIColor.orange
-        bottomButton.frame = CGRect(x: 100, y: 500, width: 200, height: 50)
+        bottomButton.frame = CGRect(x: 100, y: 750, width: 200, height: 50)
+        
+        helloButton.frame.origin.y = 200
+        print("After Explicit setting Axis", helloButton.frame.origin.y)
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        //call random position
+        randomLabelPosition()
+        
+    }
+    
+    
+    @IBAction func randomButtons(_ sender: Any) {
+        i += 1
+        print("Button Pressed x" , i )
+        randomLabelPosition()
+        let asse =  2324
+        print("AGbnwr", asse)
+    }
+    
+    
+    
+    
+    func randomLabelPosition () {
+    
+        // setting random x & y values
+        let xRandom = Int(arc4random_uniform(700) + 1)
+        let yRandom = Int(arc4random_uniform(400) + 1)
+        
+        //setting the button x & y axis
+        helloButton.frame.origin.y = CGFloat(xRandom)
+        helloButton.frame.origin.x = CGFloat(yRandom)
+        print("After Explicit setting Axis", helloButton.frame.origin.y , helloButton.frame.origin.x)
+        
     }
 }
