83967071e51686f4a703af28d14aed2ae1dcb993
Kautilya <kautilyasave@gmail.com>
2018-11-16 11:05:01 +0530

    Initial Commit.

---
+++ VSAppCenter/VSAppCenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VSAppCenter
+//
+//  Created by Kautilya on 16/11/18.
+//  Copyright © 2018 Kautilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
