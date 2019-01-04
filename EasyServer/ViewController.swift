5c9cf89c36048260ebfa25490aab7ada8cc1c7c5
Kautilya <kautilyasave@gmail.com>
2019-01-04 12:15:34 +0530

    Initial Commit

---
+++ EasyServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyServer
+//
+//  Created by Kautilya on 04/01/19.
+//  Copyright © 2019 Kautilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
