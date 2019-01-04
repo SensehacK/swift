8d7de4fdf734b3bed2e7e478ca4ca97d94698e86
Kautilya <kautilyasave@gmail.com>
2019-01-04 14:56:45 +0530

    Added Chef & Server Screens.

--- EasyServer/ViewController.swift
+++ EasyServer/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var welcomeView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("button pressed")
+        
+    }
+    
+    
 }
 
