9a130b61faeea4dadfd179ad412abf3784c48d4b
Kautilya <kautilyasave@gmail.com>
2019-01-07 16:04:17 +0530

    Initial Commit

---
+++ ParseJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseJSON
+//
+//  Created by Kautilya on 07/01/19.
+//  Copyright © 2019 Kautilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
