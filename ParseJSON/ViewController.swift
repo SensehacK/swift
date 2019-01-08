1dbf0924051d4350fa0f0bfeadfca0b36d3c8c76
Kautilya <kautilyasave@gmail.com>
2019-01-08 18:04:51 +0530

    Updated New Data with full restaurant menu.

--- ParseJSON/ViewController.swift
+++ ParseJSON/ViewController.swift
@@ -20,7 +20,10 @@ class ViewController: UIViewController {
                 let data2 = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
                 let jsonResult = try JSONSerialization.jsonObject(with: data2 , options: .mutableLeaves)
                 if let jsonResult2 = jsonResult as? Dictionary<String, AnyObject>, let restaurant = jsonResult2["restaurant-info"] as? [Any] {
-                    print(restaurant)
+                    // Printing full stack of JSON.
+                    //                        print(restaurant)
+                    
+                    // Printing diff parameters from JSON.
 //                    let restType = restaurant["name"] as String
 //                    let restTime = restaurant["avg_delivery_time"] as String
 //                    let restDeliveryFee = restaurant["delivery-fee"] as String
@@ -39,7 +42,9 @@ class ViewController: UIViewController {
                     let data2 = try Data(contentsOf: URL(fileURLWithPath: path2), options: .mappedIfSafe)
                     let jsonResult = try JSONSerialization.jsonObject(with: data2 , options: .mutableLeaves)
                     if let jsonResult2 = jsonResult as? Dictionary<String, AnyObject>, let restaurant = jsonResult2["person"] as? [Any] {
-                        print(restaurant)
+                        
+                        // Printing full stack of JSON.
+//                        print(restaurant)
                     }
                     
                 } catch {
@@ -94,13 +99,24 @@ class ViewController: UIViewController {
                                 print((data2 as AnyObject).contains("Appetizers"))
                             }
                             
+                            if let menuItems = data["menu-items"] as? [[String:Any]] {
+                                for menu in menuItems {
+                                    print("-----452424242-----------")
+                                    print(menu["name"] ?? "Food Menu")
+                                    print(menu["description"] ?? "Food Description")
+                                }
+
+                            }
+                            
+                            
+                            
+                            /*
+                            
                             // Condition to check specific names
                             if ((data["name"] as? String) == "Appetizers") ||  ((data["name"] as? String) == "Sharing platters") {
                                 print("--------------------")
                                 print(data["id"] ?? "No ID Received")
                                 print(data["name"] ?? "No name Received")
-
-                                
                             }
                             
                             // bit efficient code v1
@@ -108,8 +124,6 @@ class ViewController: UIViewController {
                                 print("----------^^^^^^^----------")
                                 print(data["id"] ?? "No ID Received")
                                 print(data["name"] ?? "No name Received")
-                                
-                                
                             }
                             
                             // bit efficient code v2
@@ -117,23 +131,13 @@ class ViewController: UIViewController {
                                 print("----------^^^^^%%%%%%%^^^^^----------")
                                 print(data["id"] ?? "No ID Received")
                                 print(data["name"] ?? "No name Received")
-                                
-                                
                             }
                             
                             
-                        }
-//                        if let dataRRS = categoryData["name"] {
-//                            print(dataRRS)
-//                        }
-//
-//                        if let dataRew = dataR["payment-methods"] as? [[String:Any]] {
-//                            for raw in dataRew {
-//                                print("Sensehack")
-//                                print(raw["id"] ?? 0)
-//                                print(raw["type"] ?? "kaido")
-//                            }
-//                        }
+                            */
+                            
+                            
+                        } // data Dictionary
                     }
                     
                     
@@ -149,8 +153,9 @@ class ViewController: UIViewController {
         }
             
             // calling other extra function
-            jsonNet()
-            
+//            jsonNet()
+        jsonFullNet()
+        
         
     }
     
@@ -172,6 +177,67 @@ class ViewController: UIViewController {
                 let jsonResponse = try JSONSerialization.jsonObject(with: dataResponse
                     , options: []) as? [String:Any]
                 print(jsonResponse ?? "Failed Fetch")
+                print("Sensehack ")
+                if let dataR = jsonResponse?["restaurant-info"] as? [String:Any] {
+                    if let dataRRS = dataR["name"] as? String {
+                        print(dataRRS)
+                    }
+                    
+                    if let dataRew = dataR["payment-methods"] as? [[String:Any]] {
+                        for raw in dataRew {
+                            print("Sensehack")
+                            print(raw["id"] ?? 0)
+                            print(raw["type"] ?? "kaido")
+                        }
+                    }
+                }
+            }
+            catch {
+                print("JSON parsing error")
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    func jsonFullNet () {
+        
+        let urlJSON = URL(string: "https://api.jsonbin.io/b/5c3495ae7b31f426f8537101")
+        
+        //        var request = URLRequest(url: urlJSON!)
+        //        request.httpMethod = "GET"
+        
+        
+        let task = URLSession.shared.dataTask(with: urlJSON!) { (data, response, error) in
+            guard let dataResponse = data, error == nil else {
+                print("error")
+                return
+            }
+            
+            do {
+                let jsonResponse = try JSONSerialization.jsonObject(with: dataResponse
+                    , options: []) as? [String:Any]
+                
+                if let categoryData = jsonResponse?["category"] as? [[String:Any]] {
+                    for data in categoryData {
+                        print(data["name"] ?? "Food Type")
+                        
+                        if let data2 = data["name"] {
+                            print((data2 as AnyObject).contains("Appetizers"))
+                        }
+                        
+                        if let menuItems = data["menu-items"] as? [[String:Any]] {
+                            for menu in menuItems {
+                                print("-----452424242-----------")
+                                print(menu["name"] ?? "Food Menu")
+                                print(menu["description"] ?? "Food Description")
+                            }
+                            
+                        }
+                    } // data Dictionary
+                }
+//                print(jsonResponse ?? "Failed Fetch")
                 print("Sensehack ")
                 if let dataR = jsonResponse?["restaurant-info"] as? [String:Any] {
                     if let dataRRS = dataR["name"] as? String {
