9f1e053f10713d944c1fc8949250935fddd2fc7b
Kautilya <kautilyasave@gmail.com>
2019-02-07 13:16:07 +0530

    Updated code with Swift Lint Fixes.

--- loginAuthentication/ViewController.swift
+++ loginAuthentication/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import LocalAuthentication
 
-
 class ViewController: UIViewController {
 
     @IBOutlet weak var statusLabel: UILabel!
@@ -19,66 +18,58 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
     @IBAction func loginBtnPressed(_ sender: Any) {
-        
         statusLabel.text = "Login Button Pressed"
     }
+
     @IBAction func touchIDAction(_ sender: Any) {
-        
         print("Hello Sensehack")
-        
         let myContext = LAContext()
-        let myLocalizedAuthString = "Biometric Authentication test!"
-        var authError : NSError?
+        let myLocAuthStr = "Biometric Authentication test!"
+        var authError: NSError?
         if #available(iOS 8.0, macOS 10.12.1, *) {
-            
-            // deviceOwnerAuthentication used parallely with Passcode or just want Touch ID then use deviceOwnerAuthenticationWithBiometrics
+            // deviceOwnerAuthentication used parallely with Passcode or
+            // just want Touch ID then use deviceOwnerAuthenticationWithBiometrics
             if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
-                myContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: myLocalizedAuthString) { success, evaluateError in
-                    
+                myContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: myLocAuthStr) { success, error
+                    in
                     DispatchQueue.main.async {
                         if success {
                             // user authenticated
-                            
                             print("Authenticated successfully")
                             self.statusLabel.text = "Authenticated successfully"
                             self.view.backgroundColor = UIColor.green
-                            
                         } else {
-                            
                             // user not authenticated
                             print("not authenticated")
                             self.statusLabel.text = "not authenticated"
                             self.view.backgroundColor = UIColor.red
+                            print("Printing error if any")
+                            print(error!)
                         }
                     }
-                    
                 }
             } else {
                 // couldnt evaluate policy
                 print("couldn't evaluate policy")
                 self.statusLabel.text = "couldn't evaluate policy"
-                
             }
         } else {
-            
             print("feature not supported iOS version less than 8")
             self.statusLabel.text = "feature not supported iOS version less than 8"
         }
     }
-    
-    
 }
 
 // Put this piece of code anywhere you like
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                 action: #selector(UIViewController.dismissKeyboard))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
-    
+
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
