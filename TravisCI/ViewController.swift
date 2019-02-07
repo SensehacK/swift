8ebc79b9400d33aa06163e1f8e81e870c5e745f8
Kautilya <kautilyasave@gmail.com>
2019-02-07 21:17:19 +0530

    Initial Commit

---
+++ TravisCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCI
+//
+//  Created by Sensehack on 07/02/19.
+//  Copyright © 2019 Sensehack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TravisCITests/TravisCITests.swift
@@ -0,0 +1,34 @@
+//
+//  TravisCITests.swift
+//  TravisCITests
+//
+//  Created by Sensehack on 07/02/19.
+//  Copyright © 2019 Sensehack. All rights reserved.
+//
+
+import XCTest
+@testable import TravisCI
+
+class TravisCITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
