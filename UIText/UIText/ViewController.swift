e363b19e3a25d529f5914970097ea1e8c42ed91f
Kautilya <kautilyasave@gmail.com>
2021-04-01 11:52:09 -0500

    (feat)UIText: trial

---
+++ UIText/UIText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIText
+//
+//  Created by Kautilya on 07/04/17.
+//  Copyright © 2017 Kautilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
