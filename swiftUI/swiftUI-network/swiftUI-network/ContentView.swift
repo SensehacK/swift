53fd3471347311ca02f1a32f6b72cf421eec77da
Kautilya Save <kautilyasave@gmail.com>
2023-04-14 11:44:29 -0600

    swiftUI - added alert for failing network request

--- swiftUI/swiftUI-network/swiftUI-network/ContentView.swift
+++ swiftUI/swiftUI-network/swiftUI-network/ContentView.swift
@@ -20,44 +20,60 @@ struct ContentView: View {
     
     
     @State var todoList: [TODO] = []
-    
+    @State private var presentAlert = false
+    @State private var alertMessage = ""
     
     var body: some View {
         
         
-        VStack {
-            Text("Hello")
-            List(todoList) { todo in
-                Text(todo.title)
-            }
-            
-        }
-        .onAppear {
-            
-            guard let url = URL(string: Constants.factsURL) else {
-                print("Not able to create URL!")
-                return
-            }
-            Webservice().fetch(url: url) { data in
-                return try! JSONDecoder().decode([TODO].self, from: data)
-            } completion: { result in
-                switch result {
-                    
-                case .success(let todos):
-                    guard let todos = todos else { return }
-                    DispatchQueue.main.async {
-                        self.todoList = todos
-                    }
-
-                case .failure(let error):
-                    print("Error \(error)")
+        NavigationView {
+            VStack {
+                List(todoList) { todo in
+                    Text(todo.title)
                 }
             }
-
-            
-            
+            .navigationTitle(Constants.title)
+            .onAppear {
+                guard let url = URL(string: Constants.factsURL) else {
+                    print("Not able to create URL!")
+                    return
+                }
+                Webservice().fetch(url: url) { data -> [TODO]? in
+                    do {
+                        return try JSONDecoder().decode([TODO].self, from: data)
+                    } catch {
+                        alertMessage = "Error while decoding network response. Change in network endpoint contract!"
+                        presentAlert.toggle()
+                        return nil
+                    }
+                } completion: { result in
+                    switch result {
+                    case .success(let todos):
+                        guard let todosU = todos else { return }
+                        DispatchQueue.main.async {
+                            self.todoList = todosU
+                        }
+                    case .failure(let error):
+//                        print("Error \(error)")
+                        switch error {
+                        case .requestBad:
+                            alertMessage = "Bad network endpoint. Error while reaching network url!"
+                        case .requestDecodingFailed:
+                            alertMessage = "Error while decoding network response."
+                        }
+                        presentAlert.toggle()
+                    }
+                }
+            }
+            .alert("Title", isPresented: $presentAlert, actions: {
+                Button("Cancel", role: .cancel, action: {})
+            }, message: {
+                Text(alertMessage)
+            })
         }
+        
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -69,4 +85,5 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Constants {
     static let factsURL: String = "https://jsonplaceholder.typicode.com/todos"
+    static let title = "TODO list API"
 }
