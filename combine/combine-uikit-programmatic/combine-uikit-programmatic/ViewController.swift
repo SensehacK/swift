0877307b0a69ef0c5258d406fad0479b260f251c
Kautilya Save <kautilyasave@gmail.com>
2023-04-18 17:26:18 -0600

    combine - uikit: removed storyboard xcode

---
+++ combine/combine-uikit-programmatic/combine-uikit-programmatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  combine-uikit-programmatic
+//
+//  Created by Kautilya Save on 4/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
