b903644cd73f0d435a976a7f18e29b7e40a87f24
Kautilya Save <kautilyasave@gmail.com>
2023-05-07 13:54:04 -0600

    mvvm loosely coupled example initial commit

---
+++ lifecycle/MVVM-DI/MVVM-DI/MVVM_DIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_DIApp.swift
+//  MVVM-DI
+//
+//  Created by Kautilya Save on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_DIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
