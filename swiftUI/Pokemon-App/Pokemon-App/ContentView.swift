fb2b5a8cadc248d6c4f6288ebd0a664cc7ac1d0b
Kautilya Save <kautilyasave@gmail.com>
2023-05-11 09:20:35 -0600

    init pokemon app

---
+++ swiftUI/Pokemon-App/Pokemon-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pokemon-App
+//
+//  Created by Kautilya Save on 5/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI/Pokemon-App/Pokemon-App/Pokemon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_AppApp.swift
+//  Pokemon-App
+//
+//  Created by Kautilya Save on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
